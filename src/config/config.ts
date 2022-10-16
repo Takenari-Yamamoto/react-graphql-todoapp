d64e82a3cf2bbc3c3626ef8234645ee98ae91717
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-10-16 16:52:24 +0900

    auth0 provider 追加

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,9 @@
+/* Hasura */
 export const API_URL = process.env.REACT_APP_HASURA_URL as string;
 export const API_KEY = process.env.REACT_APP_HASURA_KEY as string;
+
+/* Auth0*/
+export const AUTH_ZERO_DOMAIN = process.env
+  .REACT_APP_AUTH_ZERO_DOMAIN as string;
+export const AUTH_ZERO_CLIENT_ID = process.env
+  .REACT_APP_AUTH_ZERO_CLIENT_ID as string;
