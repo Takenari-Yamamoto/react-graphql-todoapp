32b8d845cfa28b4fbf4daa88868900bf67473c54
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-10-16 15:59:01 +0900

    フォーム作った

--- src/features/auth/page/AuthPage.tsx
+++ src/features/auth/page/AuthPage.tsx
@@ -1,8 +1,34 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import style from '../page/authPage.module.css';
 
 const AuthPage = () => {
-  return <div className={style.container}>auth</div>;
+  const [formType, setType] = useState<'login' | 'register'>('login');
+  const handleSetType = () => {
+    if (formType === 'login') {
+      setType('register');
+    } else {
+      setType('login');
+    }
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.formContainer}>
+        <label htmlFor="login" className={style.form}>
+          ユーザーID
+          <input type="text" name="login" />
+        </label>
+        <label htmlFor="password" className={style.form}>
+          パスワード
+          <input type="text" name="password" />
+        </label>
+        <button>{formType === 'login' ? 'ログイン' : '会員登録'}</button>
+        <p className={style.text} onClick={() => handleSetType()}>
+          {formType === 'login' ? 'アカウントないよ！' : 'アカウントあるよ！'}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default memo(AuthPage);
