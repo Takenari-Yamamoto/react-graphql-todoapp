d86763bdc616a206b0668e66c4c382f3bbe1bc4e
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2022-10-16 15:44:16 +0900

    auth page 作成

---
+++ src/features/auth/page/AuthPage.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react';
+import style from '../page/authPage.module.css';
+
+const AuthPage = () => {
+  return <div className={style.container}>auth</div>;
+};
+
+export default memo(AuthPage);
